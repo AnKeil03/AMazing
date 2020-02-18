dedde6befd0565881aca4e77e63b415543bc1539
klsteele <kevin-steele@uiowa.edu>
2020-02-17 23:58:38 -0600

    Spring gradle

--- MacySprings/src/main/js/app.jsx
+++ MacySprings/src/main/js/app.jsx
@@ -1,17 +1,78 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+//const React = require('react');
+//const ReactDOM = require('react-dom');
 import '../webpack/css/main.css'
 //const client = require('./client');
 
 class Main extends Component {
+
     render() {
         return (
         <div id = 'Main'>
             <h1>Demo Component</h1>
+            <UserList/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
         </div>
     );
     }
+}
+
+class UserList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {users:[]};
+    }
+
+    componentDidMount(){
+        let self = this;
+        fetch('/user/all', {
+            method: 'GET'
+        }).then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        }).then(function(data) {
+            self.setState({users: data});
+        }).catch(err => {
+            console.log('caught it!',err);
+        })
+    }
+
+    render() {
+        if (!this.state.users) {
+            return <div>No Users yet...</div>
+        }
+        let users = Array.from(this.state.users)
+        return (
+            <div className="container">
+                <div className="panel panel-default p50 uth-panel">
+                    <table className="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Member name</th>
+                            <th>Member email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {users.map(member =>
+                            <tr key={member.id}>
+                                <td>{member.name} </td>
+                                <td>{member.email}</td>
+                                <td><a>Edit</a>|<a>Delete</a></td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+class User extends Component{
+
 }
 
 ReactDOM.render(
