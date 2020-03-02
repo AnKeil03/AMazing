d253b1e40abec96f86ca5945ad20debc05227ca7
rangerjammy <kevsteele7@gmail.com>
2020-03-01 21:13:15 -0600

    reverting online commit

--- MacySprings/db/user.sql
+++ MacySprings/db/user.sql
@@ -1,4 +1,2 @@
-USE seproject7;
-create user 'springuser'@'localhost' identified by 'yes';
-grant all on seproject7.* to 'springuser'@'localhost';
-select user;
+create user 'springuser'@'%' identified by 'yes';
+grant all on seproject7.* to 'springuser'@'%';
