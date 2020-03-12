cd734287cf308a839659b275971db8a1028ff332
klsteele <kevsteele7@gmail.com>
2020-03-11 22:13:23 -0500

    Changes to make Sonar Have test coverage

--- MacySprings/src/test/java/com/seProject/groupProject7/DatabaseAppTests.java
+++ MacySprings/src/test/java/com/seProject/groupProject7/DatabaseAppTests.java
@@ -2,11 +2,12 @@ package com.seProject.groupProject7;
 
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.seProject.groupProject7.login.Cryptography;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,21 @@ public class DatabaseAppTests {
                 .andExpect(content().string(containsString("email")));
     }
 
+    @Test
+    public void testCookiesByHTTP() throws Exception {
+        this.mockMvc.perform(get("/user/add?user=jkljkljkljkljkl&" +
+               "pass="+Cryptography.cipherMatchJS("poingasdflkj") +
+                "&email=me@you.com")).andDo(print());
+        this.mockMvc.perform(get("/user/get?name=jkljkljkljkljkl")).andDo(print());
+        this.mockMvc.perform(get("/user/checkLogin?cval=-1&user=jkljkljkljkljkl&" +
+                "pass="+Cryptography.cipherMatchJS("poingasdflkj")))
+                .andDo(print())
+                .andExpect(content().string(containsString("loginsuccess")));
+        this.mockMvc.perform(delete("/user/removeUser?"+"username=jkljkljkljkljkl"))
+                .andDo(print())
+                .andExpect(status().is(200));
+
+    }
 
 
 }
